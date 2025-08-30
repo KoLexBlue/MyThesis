1194fbc52c87f615f25f698188a41b77245ec22a
KoLexBlue <56235449+KoLexBlue@users.noreply.github.com>
2025-08-30 17:09:35 +0300

    Revert "ONNX Support added"
    This reverts commit 3b97d669f07bd858d470aef62969632b3127d712.

--- Source/NeuralNexusThesis/NeuralNexusThesis.Build.cs
+++ Source/NeuralNexusThesis/NeuralNexusThesis.Build.cs
@@ -1,5 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-using System.IO; // ADD THIS IMPORT FOR ONNX SUPPORT, 4.27 No plug-in found
+
 using UnrealBuildTool;
 
 public class NeuralNexusThesis : ModuleRules
@@ -8,35 +8,16 @@ public class NeuralNexusThesis : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "UMG", "RenderCore", "RHI" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        // ONNX Runtime integration from nuget
-        string BaseDir = Path.GetDirectoryName(ModuleDirectory);
-        string OnnxPath = Path.Combine(BaseDir, "ThirdParty", "ONNXRuntime");
-        string IncludePath = Path.Combine(OnnxPath, "include");
-        string LibraryPath = Path.Combine(OnnxPath, "lib");
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        PublicIncludePaths.Add(IncludePath);
-
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "onnxruntime.lib"));
-
-            // Copy DLL to output directory
-            string DllPath = Path.Combine(OnnxPath, "bin", "onnxruntime.dll");
-            RuntimeDependencies.Add("$(TargetOutputDir)/onnxruntime.dll", DllPath);
-        }
-
-        bEnableExceptions = true;
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
